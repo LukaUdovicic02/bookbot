2d57c93e46ec5856331196ecd6924d40be3a1b8c
LukaUdovicic02 <lukaudovicic10@gmail.com>
2025-08-26 15:16:16 +0200

    added the pycache to ignore and added getting the characters of a book , and sorting them

--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def get_num_words(txt):
     fico = txt.split()
     ficolen = len(fico)
-    return print(f"{ficolen} words found in the document")
+    return print(f"Found {ficolen} total words")
 
 def get_num_chars(txt):
     text_lower = txt.lower()
@@ -12,6 +12,22 @@ def get_num_chars(txt):
            char_dict[letter] += num_chars
         elif letter not in char_dict: 
             char_dict[letter] = num_chars
-
-    print(char_dict)
+    sorted_list = sort_num(char_dict)
     
+    get_num_words(text_lower)
+    for items_dict in sorted_list:
+        print(f"{items_dict["char"]}: {items_dict["num"]}")    
+
+def sort_num(char_dict):
+    sorted_list = []
+    for key,val in char_dict.items():
+        sorted_dict = {}
+        if key.isalpha():
+            sorted_dict["char"] = key
+            sorted_dict["num"] = val
+            sorted_list.append(sorted_dict) 
+    sorted_list.sort(reverse=True,key=sort_on)
+    return sorted_list
+
+def sort_on(items):
+    return items["num"]
