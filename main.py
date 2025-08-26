40920da80e47e8998edc84f2dad712dd48bfd8c1
LukaUdovicic02 <lukaudovicic10@gmail.com>
2025-08-26 18:00:56 +0200

    added system arguments in order to be able to put name of the books as arguments

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from stats import get_num_words, get_num_chars
+import sys
 
 def get_book_text():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        return file_contents
+    with open(sys.argv[1]) as f:
+        return f.read()
 
+def print_report(word_count, sorted_chars):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")  
 
 def main():
-    #get_num_words(get_book_text())
-    get_num_chars(get_book_text())
-main()
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)     
+    word_count = get_num_words(get_book_text())
+    sorted_chars = get_num_chars(get_book_text())
+    print_report(word_count,sorted_chars)
+    
+
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -1,7 +1,5 @@
 def get_num_words(txt):
-    fico = txt.split()
-    ficolen = len(fico)
-    return print(f"Found {ficolen} total words")
+    return len(txt.split())
 
 def get_num_chars(txt):
     text_lower = txt.lower()
@@ -12,20 +10,14 @@ def get_num_chars(txt):
            char_dict[letter] += num_chars
         elif letter not in char_dict: 
             char_dict[letter] = num_chars
-    sorted_list = sort_num(char_dict)
-    
-    get_num_words(text_lower)
-    for items_dict in sorted_list:
-        print(f"{items_dict["char"]}: {items_dict["num"]}")    
+    sorted_list = sort_num(char_dict)   
+    return sorted_list
 
 def sort_num(char_dict):
     sorted_list = []
     for key,val in char_dict.items():
-        sorted_dict = {}
         if key.isalpha():
-            sorted_dict["char"] = key
-            sorted_dict["num"] = val
-            sorted_list.append(sorted_dict) 
+            sorted_list.append({"char": key, "num":val}) 
     sorted_list.sort(reverse=True,key=sort_on)
     return sorted_list
 
